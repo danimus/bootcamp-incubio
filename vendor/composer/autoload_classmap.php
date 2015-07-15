f61aa82ffd0f36a29d52fa0dbaf50b48426f5f8a
Sara Galindo <sgalinma10@alumnes.ub.edu>
2015-07-15 15:49:03 +0200

    Seed added

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -441,4 +441,5 @@ return array(
     'SessionHandlerInterface' => $vendorDir . '/symfony/http-foundation/Symfony/Component/HttpFoundation/Resources/stubs/SessionHandlerInterface.php',
     'TestCase' => $baseDir . '/tests/TestCase.php',
     'Text_Template' => $vendorDir . '/phpunit/php-text-template/src/Template.php',
+    'Users' => $baseDir . '/database/migrations/2015_07_15_125517_users.php',
 );
