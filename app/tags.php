5a46ab2c026ab501ad175b7fe6b016b2de0e2acc
Andrés Pérez Sánchez <andrespes@gmail.com>
2015-07-16 18:32:40 +0200

    nada util

--- app/tags.php
+++ app/tags.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model {
+class tags extends Model {
 
 	protected $table='tags';
 
--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'mediatweet.incubio@gmail.com', 'name' => 'MediaTweet'],
 
 	/*
 	|--------------------------------------------------------------------------
