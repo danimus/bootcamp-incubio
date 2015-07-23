d3c8601ed9f7e6ff33174e364d8325f16187ed1d
Dani Álvarez <dani@danimus.com>
2015-07-23 17:35:23 +0200

    Added Node Daemon to retrieve Twitter Stream data

---
+++ daemon/node/keys.js
@@ -0,0 +1,7 @@
+var keys = {
+    "consumer_key": "YZJ9MeqWEtBjGGkZrsLZ87Daa",
+    "consumer_secret": "zHnQXIyhdjjwdYlByNAUSa34LOTbvbexAhTEu4hHw24arHoyvV",
+    "access_token_key": "3364060095-iXLZqk5SZVBWuEYC09SNKeCFYdQG8gZTGrL3hzw",
+    "access_token_secret": "Gl2bzXPXds2pnC9wuoQTSDc3lEfgYWjMRZe97LNqoQdkY"
+}
+exports.keys = keys;
