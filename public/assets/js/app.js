ccb12a25b51c90da1380021d459e2d7d401964fc
Dani Álvarez <dani@danimus.com>
2015-07-24 02:29:25 +0200

    UX Improved & Cleaned comments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,215 +1,251 @@
-var app= angular.module('mediatweet', ['ngResource','ngRoute','angular-growl','ngAnimate','ngTagsInput'],function($interpolateProvider) {
-       $interpolateProvider.startSymbol('<%');
-       $interpolateProvider.endSymbol('%>');});
+var app = angular.module('mediatweet', ['ngResource', 'ngRoute', 'angular-growl', 'ngAnimate', 'ngTagsInput'], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('<%');
+    $interpolateProvider.endSymbol('%>');
+});
 
 app.config(['growlProvider', function(growlProvider) {
-	growlProvider.globalTimeToLive(3000);
-	growlProvider.globalPosition('top-right');
+    growlProvider.globalTimeToLive(3000);
+    growlProvider.globalPosition('top-right');
 }]);
 
 angular.module('mediatweet').config(function($routeProvider) {
-	$routeProvider
-	.when('/home', {
-		templateUrl: 'templates/home.html',
-		controller: 'HomeController'
-	})
-	
-	.when('/register', {
-		templateUrl: 'templates/register.html',
-		controller: 'RegisterController'
-	})
-	.when('/about-us', {
-		templateUrl: 'templates/about_us.html',
-		controller: 'HomeController'
-	})
-	.when('/remember', {
-		templateUrl: 'templates/remember_password.html',
-		controller: 'RememberPasswordController'
-	})
-	.when('/login', {
-		templateUrl: 'templates/login.html',
-		controller:'LoginController'
-	})
-	.when('/restore', {
-		templateUrl: 'templates/restore_password.html',
-		controller:'RestoreController'
-	})
-	.when('/user-confirmation', {
-		templateUrl: 'templates/confirmation.html',
-		controller:'ConfirmationController'
-	})
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/index.html',
+        })
+        .when('/home', {
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController'
+        })
 
-	.when('/tags', {
-		templateUrl: 'templates/tags.html',
-		controller:'TagsInputController'
-	})
-	.when('/statistics', {
-		templateUrl: 'templates/d3jsexample.html',
-		controller:'StatisticsController'
-	})
-	.otherwise({
-		redirectTo: '/login'
-	});
+    	.when('/register', {
+            templateUrl: 'templates/register.html',
+            controller: 'RegisterController'
+        })
+        .when('/about-us', {
+            templateUrl: 'templates/about_us.html',
+            controller: 'HomeController'
+        })
+        .when('/remember', {
+            templateUrl: 'templates/remember_password.html',
+            controller: 'RememberPasswordController'
+        })
+        .when('/login', {
+            templateUrl: 'templates/login.html',
+            controller: 'LoginController'
+        })
+        .when('/restore', {
+            templateUrl: 'templates/restore_password.html',
+            controller: 'RestoreController'
+        })
+        .when('/user-confirmation', {
+            templateUrl: 'templates/confirmation.html',
+            controller: 'ConfirmationController'
+        })
+
+   		.when('/tags', {
+            templateUrl: 'templates/tags.html',
+            controller: 'TagsInputController'
+        })
+        .when('/statistics', {
+            templateUrl: 'templates/graph.html',
+            controller: 'StatisticsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
 
 
 /*    controllers     */
-app.controller('StatisticsController',['$scope', '$http','$location', function($scope, $http, $location){
-	
+app.controller('StatisticsController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+
 
 }]);
 
 
-app.controller('HomeController',['$scope', '$http','$location', function($scope, $http, $location){
-	$http.get('api/v1/user/nameuser').
-	success(function(data) {
-		$scope.name=data;
-		console.log(data);
-	});
-	$scope.aboutUs= function(){
-		$location.path('/about-us').replace();
-	};
+app.controller('HomeController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+    $http.get('api/v1/user/nameuser').
+    success(function(data) {
+        $scope.name = data;
+        console.log(data);
+    });
+    $scope.aboutUs = function() {
+        $location.path('/about-us').replace();
+    };
 
 }]);
 
-app.controller('ConfirmationController',['$scope','$http', 'growl', '$routeParams','$location' , function($scope, $http, growl,$routeParams,$location){
-	$scope.confirmationUser = function(){
-		$http.get('api/v1/user/confirmateemail/?token=' + $routeParams.token).
-		success(function(data) {
-			if(data.header.success == "yes"){
-				growl.success(data.header.msg,{title: 'Success message',
-					onclose: function(){ 
-						$location.path('/login').replace();
-					}
-				});
+app.controller('ConfirmationController', ['$scope', '$http', 'growl', '$routeParams', '$location', function($scope, $http, growl, $routeParams, $location) {
+    $scope.confirmationUser = function() {
+        $http.get('api/v1/user/confirmateemail/?token=' + $routeParams.token).
+        success(function(data) {
+            if (data.header.success == "yes") {
+                growl.success(data.header.msg, {
+                    title: 'Success message',
+                    onclose: function() {
+                        $location.path('/login').replace();
+                    }
+                });
 
-			}else if(data.header.success == "no"){
-				growl.error(data.header.msg,{title: 'Error message'});
-			}
-		}).error(function(data) {
-			growl.info('Error connection, please try again',{title: 'Error message'});
-		});
-	}
+            } else if (data.header.success == "no") {
+                growl.error(data.header.msg, {
+                    title: 'Error message'
+                });
+            }
+        }).error(function(data) {
+            growl.info('Error connection, please try again', {
+                title: 'Error message'
+            });
+        });
+    }
 }]);
 
-app.controller('LoginController',['$scope', '$http', 'growl', '$location', function($scope, $http, growl, $location){
+app.controller('LoginController', ['$scope', '$http', 'growl', '$location', function($scope, $http, growl, $location) {
 
-	$scope.forgetPassword = function(){
-		$location.path('/remember').replace();
-	}
-	$scope.loginSubmit = function(){
-		$http.post('api/v1/user/login', $scope.user).
-		success(function(data) {
-			$success=data.header.success;
-			$message=data.header.msg;
-			if($success=="yes"){
-				growl.success($message,{
-					title:'Success message',onclose: function(){ 
-						document.location.href = "http://bootcamp.incubio.com:8080/postlogin";
-					}
-				});
-			}else{
-				growl.error($message,{title: 'Error message'});
+    $scope.forgetPassword = function() {
+        $location.path('/remember').replace();
+    }
+    $scope.loginSubmit = function() {
+        $http.post('api/v1/user/login', $scope.user).
+        success(function(data) {
+            $success = data.header.success;
+            $message = data.header.msg;
+            if ($success == "yes") {
+                growl.success($message, {
+                    title: 'Success message',
+                    onclose: function() {
+                        document.location.href = "http://bootcamp.incubio.com:8080/postlogin";
+                    }
+                });
+            } else {
+                growl.error($message, {
+                    title: 'Error message'
+                });
 
-			}
-		}).error(function(data) {
-			alert(data);
-		});
-	}
+            }
+        }).error(function(data) {
+            alert(data);
+        });
+    }
 }]);
 
-app.controller('RegisterController',function($scope, $http, growl,$location){
-	$scope.registerSubmit = function (){
-		$http.post('api/v1/user/register', $scope.user).
-		success(function(data) {
-			$success=data.header.success;
-			$message=data.header.msg;
-			if($success=="yes"){
-				growl.success($message,{title: 'Success message'
-			});
+app.controller('RegisterController', function($scope, $http, growl, $location) {
+    $scope.registerSubmit = function() {
+        $http.post('api/v1/user/register', $scope.user).
+        success(function(data) {
+            $success = data.header.success;
+            $message = data.header.msg;
+            if ($success == "yes") {
+                growl.success($message, {
+                    title: 'Success message'
+                });
 
-				growl.success("Check your mailbox to activate your account " ,{title: 'Success message',
-					onclose: function(){ 
-						$location.path('/login').replace();
-					}
-				});
-			}else{
-				growl.error($message,{title: 'Error message'});
+                growl.success("Check your mailbox to activate your account ", {
+                    title: 'Success message',
+                    onclose: function() {
+                        $location.path('/login').replace();
+                    }
+                });
+            } else {
+                growl.error($message, {
+                    title: 'Error message'
+                });
 
-			}
-			console.log(data.header.msg);
-		}).error(function(data) {
-			alert(data);
-		});
-	}
+            }
+            console.log(data.header.msg);
+        }).error(function(data) {
+            alert(data);
+        });
+    }
 });
 
-app.controller('RememberPasswordController',['$scope', '$http', 'growl', function($scope, $http, growl){
-	$scope.rememberPassword = function(){
-		$http.post('api/v1/user/remember-password', {email:$scope.email}).
-		success(function(data) {
-			if(data.header.success == "yes"){
-				growl.success(data.header.msg,{title: 'Success message'});
-			}else if(data.header.success == "no"){
-				growl.error(data.header.msg,{title: 'Error message'});
-			}
-		}).error(function(data) {
-			growl.info('Error connection, please try again',{title: 'Error message'});
-		});
-	}
+app.controller('RememberPasswordController', ['$scope', '$http', 'growl', function($scope, $http, growl) {
+    $scope.rememberPassword = function() {
+        $http.post('api/v1/user/remember-password', {
+            email: $scope.email
+        }).
+        success(function(data) {
+            if (data.header.success == "yes") {
+                growl.success(data.header.msg, {
+                    title: 'Success message'
+                });
+            } else if (data.header.success == "no") {
+                growl.error(data.header.msg, {
+                    title: 'Error message'
+                });
+            }
+        }).error(function(data) {
+            growl.info('Error connection, please try again', {
+                title: 'Error message'
+            });
+        });
+    }
 }]);
 
 
-app.controller('RestoreController',['$scope','$http', 'growl', '$routeParams','$location' , function($scope, $http, growl,$routeParams,$location){
-	$scope.resetSumit = function(){
-		$http.post('api/v1/user/reset-password', {token:$routeParams.token,password:$scope.password,passwordconfirmation:$scope.confirmPassword}).
-		success(function(data) {
-			if(data.header.success == "yes"){
-				growl.success(data.header.msg,{title: 'Success message',
-					onclose: function(){ 
-						$location.path('/login').replace();
-					}
-				});
+app.controller('RestoreController', ['$scope', '$http', 'growl', '$routeParams', '$location', function($scope, $http, growl, $routeParams, $location) {
+    $scope.resetSumit = function() {
+        $http.post('api/v1/user/reset-password', {
+            token: $routeParams.token,
+            password: $scope.password,
+            passwordconfirmation: $scope.confirmPassword
+        }).
+        success(function(data) {
+            if (data.header.success == "yes") {
+                growl.success(data.header.msg, {
+                    title: 'Success message',
+                    onclose: function() {
+                        $location.path('/login').replace();
+                    }
+                });
 
-			}else if(data.header.success == "no"){
-				growl.error(data.header.msg,{title: 'Error message'});
-			}
-		}).error(function(data) {
-			growl.info('Error connection, please try again',{title: 'Error message'});
-		});
-	}
+            } else if (data.header.success == "no") {
+                growl.error(data.header.msg, {
+                    title: 'Error message'
+                });
+            }
+        }).error(function(data) {
+            growl.info('Error connection, please try again', {
+                title: 'Error message'
+            });
+        });
+    }
 }]);
 
 
-app.controller('TagsInputController',function($scope,$http){
+app.controller('TagsInputController', function($scope, $http) {
 
-	$scope.ApiResultGetTags=[];
-
-
-	$http.get('/api/v1/tags/get-tags-user').
-	success(function(data) {
-		for (var i = 0; i < data.body[0].length; i++) {
-			$scope.ApiResultGetTags[i]=angular.fromJson(data.body[0][i])[0];
-
-		};
-
-	});
-	$scope.AddTagFunction=function($tag){
-
-		$http.post('/api/v1/tags/add', {tagname: $tag.tagname}).
-		success(function(data) {
-
-		});    
-  };// Finaliza la funcion AddTagFunction
+    $scope.ApiResultGetTags = [];
 
 
-  $scope.RemoveTagFunction=function($tag){
+    $http.get('/api/v1/tags/get-tags-user').
+    success(function(data) {
+        for (var i = 0; i < data.body[0].length; i++) {
+            $scope.ApiResultGetTags[i] = angular.fromJson(data.body[0][i])[0];
 
-  	$http.post('/api/v1/tags/delete', {tagname: $tag.tagname}).
-  	success(function(data) {
-  		console.log(data);
-  	});    
-  };// Finaliza la funcion AddTagFunction
+        };
+
+    });
+    $scope.AddTagFunction = function($tag) {
+
+        $http.post('/api/v1/tags/add', {
+            tagname: $tag.tagname
+        }).
+        success(function(data) {
+
+        });
+    }; // Finaliza la funcion AddTagFunction
+
+
+    $scope.RemoveTagFunction = function($tag) {
+
+        $http.post('/api/v1/tags/delete', {
+            tagname: $tag.tagname
+        }).
+        success(function(data) {
+            console.log(data);
+        });
+    }; // Finaliza la funcion AddTagFunction
 
 });
-
--- resources/views/includes/header_admin.blade.php
+++ resources/views/includes/header_admin.blade.php
@@ -1,42 +1,38 @@
 <div ng-controller="HomeController" ng-app="mediatweet" id="wrapper" >
-
-	<!-- Navigation -->
-	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#/home"><img style="width:30%;" src="<?= URL::to('/')?>/assets/images/Logo_blanco.png"></a>
-		</div>
-		<!-- Top Menu Items -->
-		<ul class="nav navbar-right top-nav">
-			<li class="dropdown" >
-
-				<a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <% name %><b class="caret"></b></a>
-				<ul class="dropdown-menu">
-					<li>
-						<a href="/user/logout"><i class="fa fa-fw fa-power-off" ></i> Log Out</a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-		<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<ul class="nav navbar-nav side-nav">
-				<li class="active">
-					<a href="#/tags"><i class="fa fa-twitter" ></i>	    Tags</a>
-				</li>
-				<li>
-					<a href="#/statistics"><i class="fa fa-bar-chart" ></i>	    Statistics</a>
-				</li>
-			</ul>
-		</div>
-		<!-- /.navbar-collapse -->
-	</nav>
+   <!-- Navigation -->
+   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#/home"><img style="width:30%;" src="<?= URL::to('/')?>/assets/images/Logo_blanco.png"></a>
+      </div>
+      <!-- Top Menu Items -->
+      <ul class="nav navbar-right top-nav">
+         <li class="dropdown" >
+            <a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <% name %><b class="caret"></b></a>
+            <ul class="dropdown-menu">
+               <li>
+                  <a href="/user/logout"><i class="fa fa-fw fa-power-off" ></i> Log Out</a>
+               </li>
+            </ul>
+         </li>
+      </ul>
+      <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+         <ul class="nav navbar-nav side-nav">
+            <li class="active">
+               <a href="#/tags"><i class="fa fa-twitter" ></i>	    Tags</a>
+            </li>
+            <li>
+               <a href="#/statistics"><i class="fa fa-bar-chart" ></i>	    Statistics</a>
+            </li>
+         </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+   </nav>
 </div>
-
-
